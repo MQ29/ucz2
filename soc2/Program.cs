ac1816f4798749dc3eac629c7ab676ada7058725
Mateusz Łukaszewicz <mateusz.lukaszewicz@microsoft.wsei.edu.pl>
2022-11-21 04:51:14 -1100

    sockety

---
+++ soc2/Program.cs
@@ -0,0 +1,45 @@
+﻿//(C) Michal Turek
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+namespace Serwer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPHostEntry host = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress ip = host.AddressList[0];
+            IPEndPoint handler = new IPEndPoint(ip, 2000);
+            Socket socket = new Socket(ip.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                socket.Bind(handler);
+                socket.Listen(20);  // max. liczba oczekujšcych
+                Console.WriteLine("Ready.");
+                while (true)
+                {
+                    Socket clientSocket = socket.Accept();
+                    byte[] buffer = new Byte[2048];
+                    string data = null;
+                    while (true)
+                    { // czytamy do pierwszej kropki
+                        int count = clientSocket.Receive(buffer);
+                        data += Encoding.ASCII.GetString(buffer, 0, count);
+                        if (data.IndexOf(".") > -1) break;
+                    }
+                    Console.WriteLine("Data from client: " + data);
+                    byte[] result = Encoding.ASCII.GetBytes("Test");
+                    clientSocket.Send(result);  // echo
+                    clientSocket.Shutdown(SocketShutdown.Both);
+                    clientSocket.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
---
+++ sock1/Program.cs
@@ -0,0 +1,36 @@
+﻿//(C) Michal Turek
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Klient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                IPHostEntry host = Dns.GetHostEntry(Dns.GetHostName());
+                IPAddress ip = host.AddressList[0];
+                IPEndPoint handler = new IPEndPoint(ip, 2000);
+                Socket socket = new Socket(ip.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+                socket.Connect(handler);
+                Console.WriteLine("Connected ", socket.RemoteEndPoint.ToString());
+                byte[] buffer = Encoding.ASCII.GetBytes("Test.");
+                int count = socket.Send(buffer);
+                byte[] result = new byte[2048];
+                int count2 = socket.Receive(result);
+                Console.WriteLine("Reply from server: " + Encoding.ASCII.GetString(result, 0, count2));
+                socket.Shutdown(SocketShutdown.Both);
+                socket.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            Console.In.ReadLine();
+        }
+    }
+}
